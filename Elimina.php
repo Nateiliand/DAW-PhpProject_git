940f36b8756e7bdfb37d087703be66fd5adb411e
Natalia <nutifry@gmail.com>
2024-04-19 12:14:41 +0200

    Documentació Nou, Elimina i canvis a Nou i Style

--- Elimina.php
+++ Elimina.php
@@ -1,4 +1,16 @@
 <?php
+    /**
+     * Script per eliminar un producte a la base de dades.
+     *
+     * Aquest script elimina un producte de la base de dades utilitzant l'ID proporcionat a través de $_GET.
+     * Si hi ha cap error, mostrarà un missatge i retornarà a la pàgina principal.
+     * 
+     * @var type $_GET
+     * 
+     * @param int $id L'ID de la categoria del producte.
+     * 
+     */
+
     // Inclou l'arxiu de connexio
     include ("Connexio.php");
 
@@ -16,7 +28,10 @@
         header("Location: index.php");
         exit();
     } else {
+        // Si hi ha algun error, mostra un missatge d'error i redirecciona a l'index
         echo "Error en eliminar el producte: " . mysqli_error($connexio);
+        header("Location: index.php");
+        exit();
     }   
 
     // Tanca la connexió a la base de dades
--- Nou.php
+++ Nou.php
@@ -12,31 +12,31 @@
     </header>
     
     <main>
-        <div class="form">
-            <hr>
-            <form method="post" autocomplete="on">
-                <fieldset>
-                    <!--nom-->
-                    <label for="nom">Nom:</label>
-                        <input type="text" name="nom" id="nom" required>
-                    <!--cognoms-->
-                    <label for="descripció">Descripció:</label>
-                        <input type="text" name="descripció" id="descripció" required>
-                    <!--telefon-->
-                    <label for="preu">Preu:</label>
-                        <input type="number" name="preu" id="preu" step="0.01" required>
-                    <!--correu-->
-	    			<label for="categoria_id">Categoría:</label><br>
-	    				<select name="categoria_id" required>
-	    					<option value="1">Elecrònics</option>
-	    					<option value="2">Roba</option>
-	    				</select>
-                    <hr>
-                    <!--Botons-->
-                    <input type="submit" value="Afegir">
-                </fieldset>
-            </form>
-        </div>
+        <hr>
+        <form method="post" autocomplete="on">
+            <fieldset>
+                <!--nom-->
+                <label for="nom">Nom:</label>
+                    <input type="text" name="nom" id="nom" required>
+                <!--cognoms-->
+                <label for="descripció">Descripció:</label>
+                    <input type="text" name="descripció" id="descripció" required>
+                <!--telefon-->
+                <label for="preu">Preu:</label>
+                    <input type="number" name="preu" id="preu" step="0.01" required>
+                <!--correu-->
+	    	<label for="categoria_id">Categoría:</label><br>
+                    <select name="categoria_id" id="categoria_id" required>
+                        <option value="" disabled selected hidden>Selecciona una categoría</option>
+	    		<option value="1">Elecrònics</option>
+	    		<option value="2">Roba</option>
+                    </select>
+                <hr>
+                <!--Botons-->
+                <input type="submit" value="Afegir">
+            </fieldset>
+        </form>
+
     </main>
 
     <footer>
@@ -45,11 +45,26 @@
 </html>
 
 <?php
-// Recull les dades enviades pel formulari
-    $nom = $_POST["nom"];
-    $descripció = $_POST["descripció"];
-    $preu = $_POST["preu"];
-    $categoria_id = $_POST["categoria_id"];
+
+    /**
+     * Script per afegir un producte a la base de dades.
+     *
+     * Aquest script recull les dades enviades per un formulari (nom, descripció, preu i categoria del producte)
+     * i les insereix a la base de dades de productes.
+     * Si hi ha cap error, mostrarà un missatge i retornarà a la pàgina principal.
+     * 
+     * @param string $nom El nom del producte.
+     * @param string $descripció La descripció del producte.
+     * @param float $preu El preu del producte.
+     * @param int $categoria_id L'ID de la categoria del producte.
+     * 
+     */
+
+    // Recull les dades enviades pel formulari
+    $nom = filter_input(INPUT_GET,"nom") ?? null;
+    $descripció = filter_input(INPUT_GET, "descripció") ?? null;
+    $preu = filter_input(INPUT_GET,"preu") ?? null;
+    $categoria_id = filter_input(INPUT_GET,"categoria_id") ?? null;
 
     // Inclou l'arxiu de connexio
     include ("Connexio.php");
@@ -65,8 +80,10 @@
         header("Location: index.php");
         exit();
     } else {
-        // Si hi ha algun error, mostra un missatge d'error.
+        // Si hi ha algun error, mostra un missatge d'error i redirecciona a l'index
         echo "Error en afegir el producte: " . mysqli_error($connexio);
+        header("Location: index.php");
+        exit();
     }   
 
     // Tanca la connexió a la base de dades
